ea7e3a21854173abc41efa1a272247e75140a654
Sasha <vasilieva.aleksandra@bk.ru>
2019-12-21 22:05:06 +0300

    добавляет стили главной страницы

--- source/js/script.js
+++ source/js/script.js
@@ -1,27 +1,30 @@
-var buttonT = document.querySelector(".navigation__toggle");
-var menuNav = document.querySelector(".navigation__menu");
-var menuUser = document.querySelector(".site-list");
+var buttonT = document.querySelector(".navigation__toggle")
+var menuNav = document.querySelector(".navigation__menu")
+var menuUser = document.querySelector(".site-list")
 
 window.onload = function() {
     menuNav.classList.add("navigation__menu--closed");
     menuUser.classList.add("site-list--closed");
-    this.buttonT.classList.remove("navigation__toggle--opened");
+    buttonT.classList.add("navigation__toggle--closed");
+    buttonT.classList.remove("navigation__toggle--opened");
   };
 
   buttonT.addEventListener("Touch", function(evt) {
     evt.preventDefault();
-    if (buttonT.contains(!"navigation__toggle--opened")) {
-      buttonT.remove("navigation__toggle--opened");
-      menuNav.add("navigation__menu--closed");
-      menuUser.classList.add("site-list--closed");
+    if (buttonT.contains("navigation__toggle--closed")) {
+      buttonT.remove("navigation__toggle--closed");
+      buttonT.add("navigation__toggle--opened");
+      menuNav.remove("navigation__menu--closed");
+      menuUser.classList.remove("site-list--closed");
     } else {
       var show = function show() {
-        buttonT.add("navigation__toggle--opened");
-        menuNav.remove("navigation__menu--closed");
-        menuUser.classList.remove("site-list--closed");
+        buttonT.remove("navigation__toggle--opened");
+        buttonT.add("navigation__toggle--closed");
+        menuNav.add("navigation__menu--closed");
+        menuUser.classList.add("site-list--closed");
       };
   }
-    buttonT.add("navigation__toggle--opened");
-    menuNav.remove("navigation__menu--closed");
-    menuUser.classList.remove("site-list--closed");
+    // buttonT.remove("navigation__toggle--opened");
+    // menuNav.add("navigation__menu--closed");
+    // menuUser.classList.add("site-list--closed");
 });

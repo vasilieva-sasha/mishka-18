5be63e0ea7d27f22b4d0a52bd096e68961582248
Sasha <vasilieva.aleksandra@bk.ru>
2020-01-02 16:03:48 +0300

    исправляет ошибки пятого задания

--- source/js/script.js
+++ source/js/script.js
@@ -5,26 +5,12 @@ var menuUser = document.querySelector(".site-list")
 window.onload = function() {
     menuNav.classList.add("navigation__menu--closed");
     menuUser.classList.add("site-list--closed");
-    buttonT.classList.add("navigation__toggle--closed");
     buttonT.classList.remove("navigation__toggle--opened");
   };
 
-  buttonT.addEventListener("Touch", function(evt) {
+  buttonT.addEventListener("click", function(evt) {
     evt.preventDefault();
-    if (buttonT.contains("navigation__toggle--closed")) {
-      buttonT.remove("navigation__toggle--closed");
-      buttonT.add("navigation__toggle--opened");
-      menuNav.remove("navigation__menu--closed");
-      menuUser.classList.remove("site-list--closed");
-    } else {
-      var show = function show() {
-        buttonT.remove("navigation__toggle--opened");
-        buttonT.add("navigation__toggle--closed");
-        menuNav.add("navigation__menu--closed");
-        menuUser.classList.add("site-list--closed");
-      };
-  }
-    // buttonT.remove("navigation__toggle--opened");
-    // menuNav.add("navigation__menu--closed");
-    // menuUser.classList.add("site-list--closed");
-});
+    buttonT.classList.toggle("navigation__toggle--opened")
+    menuNav.classList.toggle("navigation__menu--opened")
+    menuUser.classList.toggle("site-list--opened")
+  });
